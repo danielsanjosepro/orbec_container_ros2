37daba2259334f747938d4b7a36ac310a1fa9fb1
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2025-10-06 11:58:17 +0200

    Add changes for multiple cameras

--- set_cyclone.sh
+++ set_cyclone.sh
@@ -1,2 +1,2 @@
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export CYCLONEDDS_URI=file:///home/ros/ros2_ws/src/lsy_franka/cyclone_config.xml
+export CYCLONEDDS_URI=file:///home/ros/ros2_ws/src/cyclone_config.xml
